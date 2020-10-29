cb8f3e4059214010ba8a2cf736a2b5f0b69f97ff
Rodrigo <rodrigo.ulloa22@gmail.com>
2020-10-29 12:52:53 -0300

    modify user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_145250) do
+ActiveRecord::Schema.define(version: 2020_10_29_153721) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_145250) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
